51fdf99ddd6e941629349d18fe349a273350db52
Jon Perkins <jjperk@gmail.com>
2016-06-05 17:57:39 +0000

    Add language to production for devise confirm send via heroku

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,7 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
+
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV["SENDGRID_USERNAME"],
   :password => ENV["SENDGRID_PASSWORD"],
